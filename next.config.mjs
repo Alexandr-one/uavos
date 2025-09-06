03495dd7123418dfa2504ea909fd5faca1e0d8dd
Alexanrd-one <2004sasharyzhakov@gmail.com>
2025-09-06 21:25:53 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 import nextra from 'nextra'
 
 const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
   search: { codeblocks: false }
 })
 
 /** @type {import('next').NextConfig} */
 export default withNextra({
   reactStrictMode: true,
-  devIndicators: false,
-  output: 'export',        // статический экспорт
-  images: { unoptimized: true },
+  devIndicators: { buildActivity: false }, // исправлено под Next 14
+  output: 'export',       // статический экспорт
   trailingSlash: true,
-
-  // 🚀 добавь эти строки
-  basePath: '/uavos',
-  assetPrefix: '/uavos/',
+  images: { unoptimized: true },
+  distDir: 'out'          // папка сборки для gh-pages
 })
