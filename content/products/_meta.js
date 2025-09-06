4100da35589c7793b8c92dfa899964994a1b371f
Alexanrd-one <2004sasharyzhakov@gmail.com>
2025-09-06 21:08:14 +0300

    first commit

---
+++ content/products/_meta.js
@@ -0,0 +1,13 @@
+export default {
+    asc: "Automatic Control System (Autopilots)",
+    vto: "Vertical Take-Off and Landing UAVs",
+    fw: "Fixed-wing UAVs",
+    gcu: "Ground Control Units (GCU)",
+    sd: "Servo Drives",
+    uav: "UAV Engines",
+    cs: "Communication Systems",
+    gimbals: "Gimbals",
+    ptp: "Pan-Tilt Platforms",
+    dps: "Drone Parachute Systems",
+    uavp: "UAV Parts"
+}
---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  search: { codeblocks: false }
+})
+
+/** @type {import('next').NextConfig} */
+export default withNextra({
+  reactStrictMode: true,
+  devIndicators: false,
+  output: 'export',        // статический экспорт
+  images: { unoptimized: true },
+  trailingSlash: true,
+
+  // 🚀 добавь эти строки
+  basePath: '/uavos',
+  assetPrefix: '/uavos/',
+})
