1493b2e574d2b3c3227e52e9003487f35114cd9f
Alexanrd-one <2004sasharyzhakov@gmail.com>
2025-09-09 16:24:40 +0300

    develop products

--- out/_next/static/build-1757420315385/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5b7879002b4dbbde.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
